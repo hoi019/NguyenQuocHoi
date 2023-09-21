4567a59d5e8787af0da933c8850e73c420e7d150
hoideptraivodoibadao123 <134429276+hoideptraivodoibadao123@users.noreply.github.com>
2023-09-21 19:42:25 +0700

    Add project files.

---
+++ DM/AccountsMD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM
+{
+    public class AccountsMD
+    {
+        public int Id { get; set; }
+        public string AName { get; set; }
+        public string APassword { get; set; }
+        public string AType { get; set; }
+    }
+}
---
+++ DM/ManufacturesMD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD
+{
+    public class ManufacturesMD
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string MName { get; set; }
+        public string MPhone { get; set; }
+        public string MAddress { get; set; }
+        public string MEmail { get; set; }
+    }
+}
